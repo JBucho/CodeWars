3393d56a0744cb5639cf8a765c3388d843783c2f
Kuba <jakub@buchocki.pl>
2019-01-14 15:20:40 +0100

    update of simple_pig_latin.py

--- 5 kyu/simple_pig_latin.py
+++ 5 kyu/simple_pig_latin.py
@@ -1,3 +1,7 @@
+"""Move the first letter of each word to the end of it, then add "ay"
+to the end of the word. Leave punctuation marks untouched."""
+
+
 def pig_it(latin):
     latin = latin.split(' ')
     pig_latin = []
@@ -14,5 +18,6 @@ def pig_it2(latin):
     return ' '.join([w[1:] + w[0] + 'ay' if w.isalpha() else w for w in latin.split()])
 
 
-print(pig_it("Pig latin is cool !"))
-print(pig_it2("Pig latin is cool !"))
+if __name__ == '__main__':
+    print(pig_it("Pig latin is cool !"))
+    print(pig_it2("Pig latin is cool !"))
