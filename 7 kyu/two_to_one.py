12b9c76cae3f51cca1beaa324f707af61292d9af
Kuba <jakub@buchocki.pl>
2019-01-14 16:09:28 +0100

    new 7 kyu file

---
+++ 7 kyu/two_to_one.py
@@ -0,0 +1,18 @@
+"""Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string,
+the longest possible, containing distinct letters,
+
+each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ``` """
+
+
+import string
+
+
+def longest(s1, s2):
+    el = [i for i in string.ascii_lowercase if i in s1 or i in s2]
+    return ''.join(el)
+
+
+print(longest("aretheyhere", "yestheyarehere"))
+print(longest("abcdefghijklmnopqrstuvwxyz", "xxxxyyyyabklmopq"))
