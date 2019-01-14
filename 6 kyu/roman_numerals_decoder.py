0c2375e3929268a9bd988e886a852bc692a4821b
Kuba <jakub@buchocki.pl>
2019-01-14 15:57:22 +0100

    new 6 kyu files

---
+++ 6 kyu/roman_numerals_decoder.py
@@ -0,0 +1,34 @@
+"""Create a function that takes a Roman numeral as its argument and returns
+its value as a numeric decimal integer. You don't need to validate the form
+of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the
+number to be encoded separately, starting with the leftmost digit and skipping
+any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is
+rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI",
+uses each letter in descending order.
+
+Example:
+
+solution('XXI') # should return 21"""
+
+
+from functools import reduce
+
+
+def solution(roman):
+    dict = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
+    }
+    return reduce(lambda x, y: x - y if x >= 5 * y else x + y, list(dict[c] for c in roman[::-1]))
+
+
+print(solution('MCDXLIV'))
+print(solution('MDCLXVI'))
+print(solution('MCMXC'))
